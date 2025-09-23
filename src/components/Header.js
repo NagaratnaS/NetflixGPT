70601ea4bf12cf2556124821531eef968d6dbb35
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-09-23 11:53:18 +0530

    added constants for urls

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,14 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { addUser, removeUser } from "../utils/userSlice";
+import { LOGO } from "../utils/constants";
 
 const Header = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store?.user);
   useEffect(() => {
-    onAuthStateChanged(auth, (user) => {
+    const unSubscribe = onAuthStateChanged(auth, (user) => {
       if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/auth.user
@@ -33,6 +34,7 @@ const Header = () => {
         navigate("/");
       }
     });
+    return () => unSubscribe();
   }, []);
 
   const handleSignOut = () => {
@@ -49,11 +51,7 @@ const Header = () => {
   };
   return (
     <div className="px-8 py-2 bg-gradient-to-b from-black z-10 w-screen flex justify-between">
-      <img
-        className="w-44"
-        src="https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production_2025-08-26/consent/87b6a5c0-0104-4e96-a291-092c11350111/0198e689-25fa-7d64-bb49-0f7e75f898d2/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
-        alt="Logo"
-      />
+      <img className="w-44" src={LOGO} alt="Logo" />
       {user && (
         <button
           className="p-4 m-4 bg-red-500 text-white cursor-pointer float-right"
