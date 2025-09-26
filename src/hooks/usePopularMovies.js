318d1afdfa38e730c068280b25d1c95c2f1f2d1d
Nagaratna Hegde <nagaratnahegde@Nagaratnas-MacBook-Air.local>
2025-09-26 12:48:53 +0530

    added popular movies section

---
+++ src/hooks/usePopularMovies.js
@@ -0,0 +1,23 @@
+import { API_OPTIONS } from "../utils/constants";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addPopularMovies } from "../utils/moviesSlice";
+
+const usePopularMovies = () => {
+  // Custom hook logic here
+  const dispatch = useDispatch();
+  const getPopularMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/popular?page=1",
+      API_OPTIONS
+    );
+    const jsonData = await data.json();
+    dispatch(addPopularMovies(jsonData.results));
+  };
+
+  useEffect(() => {
+    getPopularMovies();
+  }, []);
+};
+
+export default usePopularMovies;
